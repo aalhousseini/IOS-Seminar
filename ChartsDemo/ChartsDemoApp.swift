bc367acaea03c70dde49ebb72421a8ffe33eea45
Ahmad Al Housseini <aalhousseini@deloitte.de>
2024-11-10 23:01:40 +0100

    Initial Commit

---
+++ ChartsDemo/ChartsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsDemoApp.swift
+//  ChartsDemo
+//
+//  Created by Al Housseini, Ahmad on 10.11.24.
+//
+
+import SwiftUI
+
+@main
+struct ChartsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
